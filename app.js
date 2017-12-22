a476e48575eb7324b7bf5ffc1a720f19067af20d
Tuan Chau <tuanchauict@gmail.com>
2017-12-22 19:20:14 +0700

    Add about page

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const app = new Una({
     el: 'app',
     data: {
-        activeTab: 2,
+        activeTab: 3,
         tabs: [
             {title: 'Home'},
             {title: 'KaaS Platform'},
